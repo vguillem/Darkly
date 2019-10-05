054f5b6afb8a979ca113b3c7818df0a8b700dd10
kyazdani42 <kyazdani@student.42.fr>
2019-10-05 10:51:55 +0200

    http instead of axios

--- done/hidden/Ressources/index.js
+++ done/hidden/Ressources/index.js
@@ -1,47 +1,63 @@
-const axios = require('axios');
-const url = 'http://192.168.1.28/.hidden/';
-const regex = /href="(.*?)\"/g;
+#!/usr/local/bin/node
 
+const http = require('http');
+
+const ip = require('process').argv[2];
+if (!ip) {
+  console.info("pass vm ip as argument");
+  return 1;
+}
+
+const url = `http://${ip}/.hidden/`;
+const regex = /href="(.*?)\"/g;
 
 const result = [];
 const maxRequest = 10;
 let actualRequest = 0;
 
+function fetch(url) {
+  return new Promise((res) => {
+    let data = '';
+    http.get(url, (resp) => {
+      resp.on('data', (chunk) => { data += chunk; });
+      resp.on('end', () => { res(data); });
+    });
+  });
+}
+
 const findFile = (url) => {
 
-    if (actualRequest > maxRequest) {
-        setTimeout(() => {
-            findFile(url);
-        }, 200)
-        return ;
-    }
+  if (actualRequest > maxRequest) {
+    return setTimeout(() => { findFile(url) }, 200);
+  }
 
-    actualRequest += 1;
+  actualRequest += 1;
 
-    axios.get(url)
+  fetch(url)
     .then((response) => {
 
-        actualRequest -= 1;
+      actualRequest -= 1;
 
-        const allLink = response.data.match(regex);
+      const allLink = response.match(regex);
 
-        return allLink.forEach(u => {
-            const clean = u.replace('href="', '').replace('"', "").replace('../', '');
+      return allLink.forEach(link => {
+        const clean = link.replace('href="', '').replace('"', '').replace('../', '');
+        const cleanUrl = `${url}${clean}`;
 
-            if (clean) {
-                if (clean === 'README') {
-                    axios.get(`${url}${clean}`)
-                    .then(file => {
-                        if (result.indexOf(file.data) === -1) {
-                            result.push(file.data);
-                            console.log(file.data);
-                        }
-                    })
-                } else {
-                    findFile(`${url}${clean}`);
+        if (clean) {
+          if (clean === 'README') {
+            fetch(cleanUrl)
+              .then(file => {
+                if (result.indexOf(file) === -1) {
+                  result.push(file);
+                  console.log(file);
                 }
-            }
-        });
+              })
+          } else {
+            findFile(cleanUrl);
+          }
+        }
+      });
     });
 };
 
--- done/hidden/Ressources/writeup.md
+++ done/hidden/Ressources/writeup.md
@@ -5,8 +5,9 @@
 En allant sur la page `VM_URL/robots.txt` (une page de referencement sur les webservers), nous allons essayer d'exploiter la page .hidden. \
 En allant donc sur la page `VM_URL/.hidden`, nous tombons sur un dossier avec un README et une liste de lien. Chacun de ses liens redirige vers un sous dossier du dossier precedent, tous comportant un README (avec des informations inutiles). \
  \
-Pour nous simplifier la vie, nous allons utiliser le script `scrape.sh` qui utilise wget et grep pour trouver des informations interessantes.
+Pour nous simplifier la vie, nous allons utiliser le script js `index.js` qui utilise http pour imprimer le contenu de tous les README de l'arborescence du dossier .hidden. \
+Le flag apparait au bout d'une grosse minute !
 
 ## protection
 
-Ne pas stocker des fichiers importants sur un serveur web public
+Ne pas stocker des fichiers importants sur un serveur web en public
